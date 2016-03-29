3d140312558925413d8ba17ced7c6fa478547ef8
David Van Der Beek <earlynovrock@gmail.com>
2016-03-29 13:31:07 -0500

    add persisted_balance for debit and credit accounts

--- app/models/concerns/credit_account.rb
+++ app/models/concerns/credit_account.rb
@@ -5,4 +5,11 @@ module CreditAccount
       debits.as_of(as_of).for_product(product_uuid).map(&:amount_cents).reduce(0, :+)
     ).to_f
   end
+
+  def persisted_balance(as_of: Date.current, product_uuid: nil)
+    (
+      credits.as_of(as_of).for_product(product_uuid).sum(:amount_cents) -
+      debits.as_of(as_of).for_product(product_uuid).sum(:amount_cents)
+    ).to_f
+  end
 end
--- app/models/concerns/debit_account.rb
+++ app/models/concerns/debit_account.rb
@@ -5,4 +5,11 @@ module DebitAccount
       credits.as_of(as_of).for_product(product_uuid).map(&:amount_cents).reduce(0, :+)
     ).to_f
   end
+
+  def persisted_balance(as_of: Date.current, product_uuid: nil)
+    (
+      debits.as_of(as_of).for_product(product_uuid).sum(:amount_cents) -
+      credits.as_of(as_of).for_product(product_uuid).sum(:amount_cents)
+    ).to_f
+  end
 end
