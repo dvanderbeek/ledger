0f3803ae4b733f94a13d464b2b4a02ac1fc91b56
David Van Der Beek <earlynovrock@gmail.com>
2016-03-28 22:43:01 -0500

    update seeds with example pmt plan series of txns

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,71 @@ Accounts::Revenue.create([
 Accounts::Equity.create([
   { name: :equity },
 ])
+
+Accounts::Liability.create([
+  { name: :unearned_cash },
+])
+
+
+Txn.create(
+  name: "Initial Funding",
+  date: Date.new(2014, 1, 1),
+  debits: { cash: 100000000 },
+  credits: { equity: 100000000 },
+)
+
+Txn.create(
+  name: "Issue Loan",
+  product_uuid: 2,
+  date: Date.new(2014, 12, 1),
+  debits: { principal: 40000 },
+  credits: { cash: 40000 },
+)
+
+Txn.create(
+  name: "Book Installment",
+  product_uuid: 2,
+  date: Date.new(2015, 1, 1),
+  debits: { accounts_receivable: 10000 },
+  credits: { principal: 10000 },
+)
+
+Txn.create(
+  name: "Start Payment Plan",
+  product_uuid: 2,
+  date: Date.new(2015, 1, 1),
+  debits: { principal: 10000 },
+  credits: { accounts_receivable: 10000 },
+)
+
+Txn.create(
+  name: "Process Payment",
+  product_uuid: 2,
+  date: Date.new(2015, 1, 15),
+  debits: { cash: 15000 },
+  credits: { principal: 15000 },
+)
+
+Txn.create(
+  name: "Book Installment",
+  product_uuid: 2,
+  date: Date.new(2015, 2, 1),
+  debits: { accounts_receivable: 0 },
+  credits: { principal: 0 },
+)
+
+Txn.create(
+  name: "Book Installment",
+  product_uuid: 2,
+  date: Date.new(2015, 3, 1),
+  debits: { accounts_receivable: 0 },
+  credits: { principal: 0 },
+)
+
+Txn.create(
+  name: "Book Installment",
+  product_uuid: 2,
+  date: Date.new(2015, 4, 1),
+  debits: { accounts_receivable: 7500 },
+  credits: { principal: 7500 },
+)
