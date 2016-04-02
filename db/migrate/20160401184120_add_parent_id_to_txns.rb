49f822cd124c7db01ccd70de6d0e64df16cc0221
David Van Der Beek <earlynovrock@gmail.com>
2016-04-01 19:27:20 -0500

    remove duplicate migrations

--- db/migrate/20160401184120_add_parent_id_to_txns.rb
+++
@@ -1,6 +0,0 @@
-class AddParentIdToTxns < ActiveRecord::Migration
-  def change
-    add_column :txns, :parent_id, :integer
-    add_index :txns, :parent_id
-  end
-end
--- db/migrate/20160401184328_add_type_to_txns.rb
+++
@@ -1,5 +0,0 @@
-class AddTypeToTxns < ActiveRecord::Migration
-  def change
-    add_column :txns, :type, :string
-  end
-end
