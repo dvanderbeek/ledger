3dd71fc118fe2c26cc22bc012f81a6cb4db34142
David Van Der Beek <earlynovrock@gmail.com>
2016-04-03 22:34:21 -0500

    remove persisted_balance

--- app/models/account.rb
+++ app/models/account.rb
@@ -13,20 +13,11 @@ class Account < ActiveRecord::Base
     where(name: names).map { |account| account.balance(as_of: as_of, for_product: for_product) }.reduce(:+)
   end
 
-  def self.persisted_balance(names, as_of: Date.current, for_product: nil)
-    where(name: names).map { |account| account.persisted_balance(as_of: as_of, for_product: for_product) }.reduce(:+)
-  end
-
   def self.daily_balance(names, date_range:, for_product: nil)
     DailyBalance.new(names, date_range: date_range, for_product: for_product).calculate
   end
 
   def balance(as_of: Date.current, for_product: nil)
-    increasing_entries.as_of(as_of).for_product(for_product).map(&:amount_cents).reduce(0, :+) -
-    decreasing_entries.as_of(as_of).for_product(for_product).map(&:amount_cents).reduce(0, :+)
-  end
-
-  def persisted_balance(as_of: Date.current, for_product: nil)
     entries
       .as_of(as_of)
       .for_product(for_product)
